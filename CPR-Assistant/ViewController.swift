01c14fee57d70ca21a5b6a7f3524f7571795810c
Hamza Ali <Hamza@hamzas-macbook-air.local>
2018-10-10 23:21:02 -0500

    Initial Commit

---
+++ CPR-Assistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CPR-Assistant
+//
+//  Created by Hamza Ali on 10/10/18.
+//  Copyright © 2018 Lab 4 (Hamza&Amal). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
